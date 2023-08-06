2bd0633bf80b4d0e2e7855af11710aeb4f02e8d7
Muhammad Shahzad <itzshahzad@gmail.com>
2023-08-07 00:49:19 +0500

    Code refactoring and improvements

--- Main.c
+++ Main.c
@@ -1,42 +1,86 @@
-#include "DHT22.h"
+//==============================================================================
+//
+//  main.c
+//
+//
+//==============================================================================
+//  FILE INFORMATION
+//==============================================================================
+//
+//  Source:
+//
+//  Project:    Humidity and temperature monitoring
+//
+//  Author:     M.Shahzad
+//
+//  Date:       20/03/2023
+//
+//  Revision:   1.0
+//
+//==============================================================================
+//  FILE DESCRIPTION
+//==============================================================================
+//
+//! \file
+//! This module takes care of the
+//
+//==============================================================================
+//  REVISION HISTORY
+//==============================================================================
+//  Revision: 1.0
+//
+//
 
-// using RD3 pin to DHT22 connection
-sbit  dht22_S1_pin  at RD3_bit;
-sbit  dht22_S1_pin_direction  at TRISD3_bit;
-dht22_sensor  dht22_S1_object;       // data structure sensor 1 DHT22
+//==============================================================================
+//  INCLUDES
+//==============================================================================
 
-char txt[20];
-void main() {
-  //todo digital
-  UART1_Init(9600);
-  UART1_Write_Text("System boot up \r");
-  
-  while (1){
+#include "main.h"
 
-    switch (dht22_S1_function(1))  {
-      case dht22_ok:
-        floattostr_(dht22_S1_object.temperature_,txt,1);
-        UART1_Write_Text("Temp1:");
-        UART1_Write_Text(txt);
-        floattostr_(dht22_S1_object.humedad_,txt,1);
-        UART1_Write_Text("  Hume1:");
-        UART1_Write_Text(txt);
-        break;
+//==============================================================================
+//
+//   void  main(void)
+//
+//   Author:   M. Shahzad
+//   Date:     06/08/2021
+//
+//! This function takes care of initlization and fetching DHT11 temperature and
+//! humidity vlaues and print on the serial console
+//
+//==============================================================================
 
-      case dht22_errorcheck_sum:
-        UART1_Write_Text("Eror \r");
-        break;
+void main()
+{
+	// todo digital
+	UART1_Init(9600);
+	UART1_Write_Text("System boot up \r");
 
-      case dht22_noresponse:
-        UART1_Write_Text("No respose \r");
-        break;
+	while (1)
+	{
 
-    }
+		switch (dht22_S1_function(1))
+		{
+		case dht22_ok:
+			floattostr_(dht22_S1_object.temperature_, txt, 1);
+			UART1_Write_Text("Temp1:");
+			UART1_Write_Text(txt);
 
-   UART1_Write_Text("\r");
+			floattostr_(dht22_S1_object.humedad_, txt, 1);
+			UART1_Write_Text("  Hume1:");
+			UART1_Write_Text(txt);
+			break;
 
-   delay_ms(3000); //3 seconds delay
+		case dht22_errorcheck_sum:
+			UART1_Write_Text("Eror \r");
+			break;
 
-  }
+		case dht22_noresponse:
+			UART1_Write_Text("No respose \r");
+			break;
+		}
+
+		UART1_Write_Text("\r");
+
+		delay_ms(3000); // 3 seconds delay
+	}
 }
-
--- main.h
+++ main.h
@@ -35,36 +35,22 @@
 //  INCLUDES
 //==============================================================================
 
+#include "DHT22.h"
+
 //==============================================================================
 //	GLOBAL FUNCTION PROTOTYPES
 //==============================================================================
-char mutli_dht22(char va);
-void floattostr_(float x, unsigned char *str, char precision);
-
-char dht22_S1_function(char value); // lee  informacion dels sensor temperatura y humedad
 
 //==============================================================================
 //  EXTERNAL OR GLOBAL DATA TYPES
 //==============================================================================
 
-extern dht22_sensor dht22_S1_object;
-extern sfr sbit dht22_S1_pin;
-extern sfr sbit dht22_S1_pin_direction;
+sbit dht22_S1_pin at RD3_bit;
+sbit dht22_S1_pin_direction at TRISD3_bit;
+
+dht22_sensor dht22_S1_object; // data structure sensor 1 DHT22
+char txt[20];
 
 //==============================================================================
 //  Global Structures
 //==============================================================================
-
-enum dht22_estado
-{
-	dht22_noresponse,
-	dht22_ok,
-	dht22_errorcheck_sum
-};
-
-typedef struct dht22_est
-{
-	char temp1_, temp2_, hum1_, hum2_;
-	unsigned int Temp_, hume_, checksum_;
-	float temperature_, humedad_;
-} dht22_sensor;
